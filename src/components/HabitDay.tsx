0b8d55dd10b547b4b7ec5573e2402991dd3c57b8
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2023-01-27 20:16:20 -0300

    fixed formula of DAY_SIZE

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,11 +1,12 @@
 import { TouchableOpacity, Dimensions } from "react-native";
 
 const WEEK_DAYS = 7;
-const SCREEN_HORIZONTAL_PADDING = (32 * 2) / 5;
 
-export const DAY_MARGIN_BETWEEN = 8;
+const SCREEN_HORIZONTAL_PADDING = 32 * 2;
+const DAY_MARGIN_BETWEEN = 8;
 export const DAY_SIZE =
-  Dimensions.get("screen").width / WEEK_DAYS - (SCREEN_HORIZONTAL_PADDING + 5);
+  (Dimensions.get("screen").width - SCREEN_HORIZONTAL_PADDING) / WEEK_DAYS -
+  DAY_MARGIN_BETWEEN;
 
 export function HabitDay() {
   return (
