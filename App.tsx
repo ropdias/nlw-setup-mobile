a89678a2b95c94e214820efc0ab9d6e5dc6d510a
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2023-02-02 01:39:51 -0300

    adding a notification to send to the user everyday

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import "./src/lib/dayjs";
+import { useEffect } from "react";
 import { StatusBar } from "react-native";
 import {
   useFonts,
@@ -7,9 +8,20 @@ import {
   Inter_700Bold,
   Inter_800ExtraBold,
 } from "@expo-google-fonts/inter";
+import * as Notifications from "expo-notifications";
 
 import { Loading } from "./src/components/Loading";
 import { Routes } from "./src/routes";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldShowAlert: true,
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+    };
+  },
+});
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -18,6 +30,38 @@ export default function App() {
     Inter_700Bold,
     Inter_800ExtraBold,
   });
+
+  async function scheduleNotifications() {
+    const trigger = new Date();
+    // Setting trigger to 19h
+    trigger.setHours(19);
+    trigger.setMinutes(0);
+    trigger.setSeconds(0);
+    trigger.setMilliseconds(0);
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Olá, Rodrigo! 🤩",
+        body: "Você praticou os seus hábitos hoje?",
+      },
+      trigger,
+    });
+  }
+
+  // async function getScheduleNotifications() {
+  //   const schedules = await Notifications.getAllScheduledNotificationsAsync();
+  //   console.log(schedules);
+  // }
+
+  async function cancelAllScheduleNotifications() {
+    await Notifications.cancelAllScheduledNotificationsAsync();
+  }
+
+  // We can use useEffect to cancel all notifications and re-schedule for the next day at the same time
+  useEffect(() => {
+    cancelAllScheduleNotifications();
+    // getScheduleNotifications();
+    scheduleNotifications();
+  }, []);
 
   if (!fontsLoaded) {
     return <Loading />;
